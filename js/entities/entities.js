c13e50abbf7705b5d77e13003a68f1f5826d34ba
Tristan Whitaker <tristanrileyw@gmail.com>
2015-01-23 11:30:27 -0800

    video 10 code and comment

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -17,6 +17,12 @@ game.PlayerEntity = me.Entity.extend({
 		//sets characters velocity on x and y axis
 		this.body.setVelocity(5, 20);
 
+		//adds animation when character is idle
+		this.renderable.addAnimation("idle", [78]);
+		//adds animation as character walks
+		this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+		//sets character image when we start game
+		this.renderable.setCurrentAnimation("idle");
 
 	},
 
@@ -27,14 +33,28 @@ game.PlayerEntity = me.Entity.extend({
 			//setVelocity() and multiplying it by me.timer.tick
 			//me.timer.tick makes the movement smooth
 			this.body.vel.x += this.body.accel.x * me.timer.tick;
-			//lets program use the walk animation
-		this.renderable.setCurrentAnimation("walk");
+			//flips walking animation
+			this.flipX(true);
 		}else{
 			//makes it so that character stops moving when key is not pressed
 			this.body.vel.x = 0;
 		}
+	//makes is so player does not stat out walking
+	if(this.body.vel.x !== 0){
+		//if statement that makes character walk
+		if (!this.renderable.isCurrentAnimation("walk")) {
+			//lets program use every walk animation
+			this.renderable.setCurrentAnimation("walk");
+		}
+		//player stops walking when key is not presseed
+	}else{
+		this.renderable.setCurrentAnimation("idle");
+	}
+
 		//shows updated position on screen
 		this.body.update(delta);
+		//lets animation update on fly
+		this._super(me.Entity, "update", [delta]);
 		return true;
 	}
 });
