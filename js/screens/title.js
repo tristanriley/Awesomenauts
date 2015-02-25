5d5d6ccdfe0a96761c2e69c7dedbd5d1fa5a99b0
Tristan Whitaker <tristanrileyw@gmail.com>
2015-02-25 10:44:48 -0800

    video 27 code and comment

--- js/screens/title.js
+++ js/screens/title.js
@@ -5,6 +5,32 @@ game.TitleScreen = me.ScreenObject.extend({
 	onResetEvent: function() {	
 		//inserts the title-screen image into the map
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
+		//makes the enter key into an object
+		me.input.bindKey(me.input.KEY.ENTER, "start");
+
+		me.game.world.addChild(new (me.Renderable.extend({
+			init: function(){
+				//calls super class 
+				this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+				//sets the font to arial, the size 46, and colors it white
+				this.font = new me.Font("Arial", 46, "white");
+			},
+			//function that sets up the writing
+			draw: function(renderer){
+				//inserts the message "Awesomenauts!" and sets where writing starts
+				this.font.draw(renderer.getContext(), "Awesomenauts!", 450, 130);
+				//inserts the message "Press ENTER to play!" and sets where writing starts
+				this.font.draw(renderer.getContext(), "Press ENTER to play!", 250, 530);
+			}
+		})));
+		//listens to see if enter button is pressed
+		this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge){
+			//runs if enter button is pressed
+			if (action === "start") {
+				//goes to play screen
+				me.state.change(me.state.PLAY);
+			}
+		});
 	},
 	
 	
@@ -12,6 +38,9 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		; // TODO
+		//removes the enter key's status as an object
+		me.input.unbindKey(me.input.KEY.ENTER); 
+		//inform's program not to listen for pressed enter key
+		me.event.unsubscribe(this.handler);
 	}
 });
